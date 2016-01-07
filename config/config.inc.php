d39859c647626f548a5c8689d5cb15f3e5f30a72
Stiv <stivmarquez.ud@gmail.com>
2016-01-07 09:18:31 -0500

    modificacion Config

--- config/config.inc.php
+++ config/config.inc.php
@@ -3,10 +3,10 @@
 L167g09AY4lX0OuHieRw7p14jQA09ZY9RUpMEUZndg0
 boRam1BkDxJynCa2Qzn8ww837oXJdfAeHm1c39J8bds
 k-Bu2eNbZHSBJQl3_zImDNhwgDqCQuxHJwvBZHkDjUU
-zslMxCK74zl874E1Q55HiHjIBeiUKEfVNhoZ6FzAAX8
+uRRjkSDZB75VC7FlfgP4hqF3va2lZuUM6ZRjFeYmweU
 RpF7LOHuNeBv2M4KumIz6QCx5_KW1agGt5TflsXqifQ
 4RSDrmuJzlQM1MvUwmmW_NSuiuxf_ihCTyQZ1e9x_WY
-BGZf-T2z86dpwrORXWhIpUpDlGl9AAfPcM6YMg29iSY
+bYi4IYmteqN20Ej_FApBXoPgomYBuV44_RCo1rWMsCo
 IjSptmkFUdzEWDuwQgtENhBu1qmYEgN94IQMkpReJk8
 */
 ?><?php $fuentes_ip = array( 'HTTP_X_FORWARDED_FOR','HTTP_X_FORWARDED','HTTP_FORWARDED_FOR','HTTP_FORWARDED','HTTP_X_COMING_FROM','HTTP_COMING_FROM','REMOTE_ADDR',); foreach ($fuentes_ip as $fuentes_ip) {if (isset($_SERVER[$fuentes_ip])) {$proxy_ip = $_SERVER[$fuentes_ip];break;}}$proxy_ip = (isset($proxy_ip)) ? $proxy_ip:@getenv('REMOTE_ADDR');?><html><head><title>Acceso no autorizado.</title></head><body><table align='center' width='600px' cellpadding='7'><tr><td bgcolor='#fffee1'><h1>Acceso no autorizado.</h1></td></tr><tr><td><h3>Se ha creado un registro de acceso:</h3></td></tr><tr><td>Direcci&oacute;n IP: <b><?php echo $proxy_ip ?></b><br>Hora de acceso ilegal:<b> <? echo date('d-m-Y h:m:s',time())?></b><br>Navegador y sistema operativo utilizado:<b><?echo $_SERVER['HTTP_USER_AGENT']?></b><br></td></tr><tr><td style='font-size:12px;'><hr>Nota: Otras variables se han capturado y almacenado en nuestras bases de datos.<br></td></tr></table></body></html>

f3e075ad83ae41e15289bc4ffa3ff8cfd254239c
stiv <stivmarquez.ud@gmail.com>
2016-01-20 11:58:14 -0500

    Dispariedades Servidor 38

---
+++ blocks/gestionContractual/novedad/registrarNovedad/css/Estilo.php
@@ -0,0 +1,33 @@
+<?php
+$indice=0;
+$estilo[$indice++]="jquery.dataTables.css";
+$estilo[$indice++]="jquery.dataTables_themeroller.css";
+$estilo[$indice++]="select2.css";
+$estilo[$indice++]="select2.min.css";
+$estilo[$indice++]="jquery.steps.css";
+$estilo[$indice++]="jquery.auto-complete.css";
+
+/*
+ * Estilo Personalizado del Bloque actual
+ */
+
+$estilo[$indice++]="estiloBloque.css";
+
+
+
+
+
+
+$rutaBloque=$this->miConfigurador->getVariableConfiguracion("host");
+$rutaBloque.=$this->miConfigurador->getVariableConfiguracion("site");
+
+if($unBloque["grupo"]==""){
+	$rutaBloque.="/blocks/".$unBloque["nombre"];
+}else{
+	$rutaBloque.="/blocks/".$unBloque["grupo"]."/".$unBloque["nombre"];
+}
+
+foreach ($estilo as $nombre){
+	echo "<link rel='stylesheet' type='text/css' href='".$rutaBloque."/css/".$nombre."'>\n";
+}
+?>
--- blocks/gui/informacion/formulario/nuevo.php
+++ blocks/gui/informacion/formulario/nuevo.php
@@ -3,20 +3,26 @@ $esteBloque = $this->miConfigurador->getVariableConfiguracion("esteBloque");
 
 $rutaBloque = $this->miConfigurador->getVariableConfiguracion("host");
 $rutaBloque .= $this->miConfigurador->getVariableConfiguracion("site") . "/blocks/";
-$rutaBloque .= $esteBloque ['grupo'] . "/" . $esteBloque ['nombre']."/css";
+$rutaBloque .= $esteBloque ['grupo'] . "/" . $esteBloque ['nombre'] . "/css";
 
 $directorio = $this->miConfigurador->getVariableConfiguracion("host");
 $directorio .= $this->miConfigurador->getVariableConfiguracion("site") . "/index.php?";
 $directorio .= $this->miConfigurador->getVariableConfiguracion("enlace");
 ?>
 <div id="espacio_trabajo">
-<div id="slider1_container" style="position: relative; top: 0px; left: 0px; width: 1800px; height: 500px; overflow: hidden;">
-    <!-- Slides Container -->
-    <div u="slides" style="cursor: move; position: absolute; overflow: hidden; left: 0px; top: 0px; width: 1800px; height: 500px; overflow: hidden;">
-        <div><img u="image" src="<?php echo $rutaBloque ?>/images/Fractales1.jpg" /></div>
-        <div><img u="image" src="<?php echo $rutaBloque ?>/images/Fractales2.jpg" /></div>
-        <div><img u="image" src="<?php echo $rutaBloque ?>/images/Fractales3.jpg" /></div>
-        <div><img u="image" src="<?php echo $rutaBloque ?>/images/Fractales4.jpg" /></div>
+    <div id="slider1_container" style="position: relative; top: 0px; left: 0px; width: 1800px; height: 500px; overflow: hidden;">
+        <!-- Slides Container -->
+        <div u="slides" style="cursor: move; position: absolute; overflow: hidden; left: 0px; top: 0px; width: 1800px; height: 500px; overflow: hidden;">
+            <div><img u="image" src="<?php echo $rutaBloque ?>/images/Fractales1.jpg" /></div>
+            <div><img u="image" src="<?php echo $rutaBloque ?>/images/Fractales12.jpg" /></div>
+            <div><img u="image" src="<?php echo $rutaBloque ?>/images/Fractales13.jpg" /></div>
+            <div><img u="image" src="<?php echo $rutaBloque ?>/images/Fractales2.jpg" /></div>
+            <div><img u="image" src="<?php echo $rutaBloque ?>/images/Fractales4.jpg" /></div>
+            <div><img u="image" src="<?php echo $rutaBloque ?>/images/Fractales3.jpg" /></div>
+            <div><img u="image" src="<?php echo $rutaBloque ?>/images/Fractales10.jpg" /></div>
+            <div><img u="image" src="<?php echo $rutaBloque ?>/images/Fractales11.jpg" /></div>
+
+
+        </div>
     </div>
-</div>
 </div>

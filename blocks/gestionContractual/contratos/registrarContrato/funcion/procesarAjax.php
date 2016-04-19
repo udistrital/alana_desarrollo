ab7418632e46fd8cecbff73f95ed717c6d946a1d
sergioov <sergioov@juanlara.udistritaloas.edu.co>
2016-04-19 18:01:44 -0500

    Sergio Orjuela: Se actualiza repositorio git con la ultima versión de pruebas y cambios realizados al dia 19 de abril de 2014.
    Ya registra informacion en tabla temporal, pero presenta problemma en el
    ajax

--- blocks/gestionContractual/contratos/registrarContrato/funcion/procesarAjax.php
+++ blocks/gestionContractual/contratos/registrarContrato/funcion/procesarAjax.php
@@ -1,29 +1,29 @@
 <?php
+
 use contratos\registrarContrato\Sql;
 
 $conexion = "contractual";
-$esteRecursoDB = $this->miConfigurador->fabricaConexiones->getRecursoDB ( $conexion );
+$esteRecursoDB = $this->miConfigurador->fabricaConexiones->getRecursoDB($conexion);
 
 if ($_REQUEST ['funcion'] == 'NumeroSolicitud') {
-	
-	$cadenaSql = $this->sql->getCadenaSql ( 'ConsultarNumeroNecesidades', $_REQUEST ['valor'] );
-	
-	$resultadoItems = $esteRecursoDB->ejecutarAcceso ( $cadenaSql, "busqueda" );
+
+    $cadenaSql = $this->sql->getCadenaSql('ConsultarNumeroNecesidades', $_REQUEST ['valor']);
+
+    $resultadoItems = $esteRecursoDB->ejecutarAcceso($cadenaSql, "busqueda");
 // 	var_dump($resultadoItems);exit;
 // 	$resultadoItems = $resultadoItems [0];
-	
-	echo json_encode ( $resultadoItems );
+
+    echo json_encode($resultadoItems);
 }
 if ($_REQUEST ['funcion'] == 'AlmacenarDatos') {
-	
-	$arregloDatos = json_decode($_REQUEST ['valor']) ;
-       
-        for ($i = 0; $i <= count($arregloDatos); $i++) {
-            $cadenaSql="INSERT INTO contractual.temporal_contrato (informacion_campo) Values('".$arregloDatos[$i]."'); ";
-            $resultado = $esteRecursoDB->ejecutarAcceso ( $cadenaSql, "acceso" );
-        }
-        $resultado = $esteRecursoDB->ejecutarAcceso ( $cadenaSql, "acceso" );
-        echo json_encode ( $arregloDatos );
-}
 
+    $arregloDatos = json_decode($_REQUEST ['valor']);
+    var_dump($arregloDatos);    
+    for ($i = 0; $i <= count($arregloDatos); $i++) {
+        $cadenaSql = "INSERT INTO contractual.temporal_contrato (campo_formulario,informacion_campo) Values('" . $arregloDatos[$i+1] . "', '" . $arregloDatos[$i] . "'); ";
+        $resultado = $esteRecursoDB->ejecutarAcceso($cadenaSql, "acceso");
+    }
+
+    echo json_encode(count($arregloDatos));
+}
 ?>
--- blocks/gestionContractual/contratos/registrarContrato/script/ajax.php
+++ blocks/gestionContractual/contratos/registrarContrato/script/ajax.php
@@ -78,33 +78,87 @@ $urlDatosPaso = $url . $cadena2;
 
     function almacenarInfoTemporal(pasoActual, pasoNuevo) {
         if (pasoActual == 0) {
+
             var InfoPaso0 = [];
-            InfoPaso0[0] = document.getElementById("<?php echo $this->campoSeguro('numero_contrato') ?>").value;
-            InfoPaso0[1] = document.getElementById("<?php echo $this->campoSeguro('tipo_identificacion') ?>").value;
-            InfoPaso0[2] = document.getElementById("<?php echo $this->campoSeguro('numero_identificacion') ?>").value;
-            InfoPaso0[3] = document.getElementById("<?php echo $this->campoSeguro('digito_verificacion') ?>").value;
-            InfoPaso0[4] = document.getElementById("<?php echo $this->campoSeguro('tipo_persona') ?>").value;
-            InfoPaso0[5] = document.getElementById("<?php echo $this->campoSeguro('primer_nombre') ?>").value;
-            InfoPaso0[6] = document.getElementById("<?php echo $this->campoSeguro('segundo_nombre') ?>").value;
-            InfoPaso0[7] = document.getElementById("<?php echo $this->campoSeguro('primer_apellido') ?>").value;
-            InfoPaso0[8] = document.getElementById("<?php echo $this->campoSeguro('segundo_apellido') ?>").value;
-            InfoPaso0[9] = document.getElementById("<?php echo $this->campoSeguro('genero') ?>").value;
-            InfoPaso0[10] = document.getElementById("<?php echo $this->campoSeguro('nacionalidad') ?>").value;
-            InfoPaso0[11] = document.getElementById("<?php echo $this->campoSeguro('direccion') ?>").value;
-            InfoPaso0[12] = document.getElementById("<?php echo $this->campoSeguro('correo') ?>").value;
-            InfoPaso0[13] = document.getElementById("<?php echo $this->campoSeguro('perfil') ?>").value;
-            InfoPaso0[14] = document.getElementById("<?php echo $this->campoSeguro('profesion') ?>").value;
-            InfoPaso0[15] = document.getElementById("<?php echo $this->campoSeguro('especialidad') ?>").value;
-            InfoPaso0[16] = document.getElementById("<?php echo $this->campoSeguro('tipo_cuenta') ?>").value;
-            InfoPaso0[17] = document.getElementById("<?php echo $this->campoSeguro('numero_cuenta') ?>").value;
-            InfoPaso0[18] = document.getElementById("<?php echo $this->campoSeguro('entidad_bancaria') ?>").value;
-            InfoPaso0[19] = document.getElementById("<?php echo $this->campoSeguro('tipo_configuracion') ?>").value;
-            InfoPaso0[20] = document.getElementById("<?php echo $this->campoSeguro('clase_contratista') ?>").value;
-            InfoPaso0[21] = document.getElementById("<?php echo $this->campoSeguro('identificacion_clase_contratista') ?>").value;
-            InfoPaso0[22] = document.getElementById("<?php echo $this->campoSeguro('digito_verificacion_clase_contratista') ?>").value;
-            InfoPaso0[23] = document.getElementById("<?php echo $this->campoSeguro('porcentaje_clase_contratista') ?>").value;
-            InfoPaso0[24] = document.getElementById("<?php echo $this->campoSeguro('telefono') ?>").value;
-            var arregloDatos=[];
+
+
+            if (document.getElementById("<?php echo $this->campoSeguro('numero_contrato') ?>").value != "") {
+                InfoPaso0.push(document.getElementById("<?php echo $this->campoSeguro('numero_contrato') ?>").value);
+            }
+            if (document.getElementById("<?php echo $this->campoSeguro('tipo_identificacion') ?>").value != "") {
+                InfoPaso0.push(document.getElementById("<?php echo $this->campoSeguro('tipo_identificacion') ?>").value);
+            }
+            if (document.getElementById("<?php echo $this->campoSeguro('numero_identificacion') ?>").value != "") {
+                InfoPaso0.push(document.getElementById("<?php echo $this->campoSeguro('numero_identificacion') ?>").value);
+            }
+            if (document.getElementById("<?php echo $this->campoSeguro('digito_verificacion') ?>").value != "") {
+                InfoPaso0.push(document.getElementById("<?php echo $this->campoSeguro('digito_verificacion') ?>").value);
+            }
+            if (document.getElementById("<?php echo $this->campoSeguro('tipo_persona') ?>").value != "") {
+                InfoPaso0.push(document.getElementById("<?php echo $this->campoSeguro('tipo_persona') ?>").value);
+            }
+            if (document.getElementById("<?php echo $this->campoSeguro('primer_nombre') ?>").value != "") {
+                InfoPaso0.push(document.getElementById("<?php echo $this->campoSeguro('primer_nombre') ?>").value);
+            }
+            if (document.getElementById("<?php echo $this->campoSeguro('segundo_nombre') ?>").value != "") {
+                InfoPaso0.push(document.getElementById("<?php echo $this->campoSeguro('segundo_nombre') ?>").value);
+            }
+            if (document.getElementById("<?php echo $this->campoSeguro('primer_apellido') ?>").value != "") {
+                InfoPaso0.push(document.getElementById("<?php echo $this->campoSeguro('primer_apellido') ?>").value);
+            }
+            if (document.getElementById("<?php echo $this->campoSeguro('segundo_apellido') ?>").value != "") {
+                InfoPaso0.push(document.getElementById("<?php echo $this->campoSeguro('segundo_apellido') ?>").value);
+            }
+            if (document.getElementById("<?php echo $this->campoSeguro('genero') ?>").value != "") {
+                InfoPaso0.push(document.getElementById("<?php echo $this->campoSeguro('genero') ?>").value);
+            }
+            if (document.getElementById("<?php echo $this->campoSeguro('nacionalidad') ?>").value != "") {
+                InfoPaso0.push(document.getElementById("<?php echo $this->campoSeguro('nacionalidad') ?>").value);
+            }
+            if (document.getElementById("<?php echo $this->campoSeguro('direccion') ?>").value != "") {
+                InfoPaso0.push(document.getElementById("<?php echo $this->campoSeguro('direccion') ?>").value);
+            }
+            if (document.getElementById("<?php echo $this->campoSeguro('telefono') ?>").value != "") {
+                InfoPaso0.push(document.getElementById("<?php echo $this->campoSeguro('telefono') ?>").value);
+            }
+            if (document.getElementById("<?php echo $this->campoSeguro('correo') ?>").value != "") {
+                InfoPaso0.push(document.getElementById("<?php echo $this->campoSeguro('correo') ?>").value);
+            }
+            if (document.getElementById("<?php echo $this->campoSeguro('perfil') ?>").value != "") {
+                InfoPaso0.push(document.getElementById("<?php echo $this->campoSeguro('perfil') ?>").value);
+            }
+            if (document.getElementById("<?php echo $this->campoSeguro('profesion') ?>").value != "") {
+                InfoPaso0.push(document.getElementById("<?php echo $this->campoSeguro('profesion') ?>").value);
+            }
+            if (document.getElementById("<?php echo $this->campoSeguro('especialidad') ?>").value != "") {
+                InfoPaso0.push(document.getElementById("<?php echo $this->campoSeguro('especialidad') ?>").value);
+            }
+            if (document.getElementById("<?php echo $this->campoSeguro('tipo_cuenta') ?>").value != "") {
+                InfoPaso0.push(document.getElementById("<?php echo $this->campoSeguro('tipo_cuenta') ?>").value);
+            }
+            if (document.getElementById("<?php echo $this->campoSeguro('numero_cuenta') ?>").value != "") {
+                InfoPaso0.push(document.getElementById("<?php echo $this->campoSeguro('numero_cuenta') ?>").value);
+            }
+            if (document.getElementById("<?php echo $this->campoSeguro('entidad_bancaria') ?>").value != "") {
+                InfoPaso0.push(document.getElementById("<?php echo $this->campoSeguro('entidad_bancaria') ?>").value);
+            }
+            if (document.getElementById("<?php echo $this->campoSeguro('tipo_configuracion') ?>").value != "") {
+                InfoPaso0.push(document.getElementById("<?php echo $this->campoSeguro('tipo_configuracion') ?>").value);
+            }
+            if (document.getElementById("<?php echo $this->campoSeguro('clase_contratista') ?>").value != "") {
+                InfoPaso0.push(document.getElementById("<?php echo $this->campoSeguro('clase_contratista') ?>").value);
+            }
+            if (document.getElementById("<?php echo $this->campoSeguro('identificacion_clase_contratista') ?>").value != "") {
+                InfoPaso0.push(document.getElementById("<?php echo $this->campoSeguro('identificacion_clase_contratista') ?>").value);
+            }
+            if (document.getElementById("<?php echo $this->campoSeguro('digito_verificacion_clase_contratista') ?>").value != "") {
+                InfoPaso0.push(document.getElementById("<?php echo $this->campoSeguro('digito_verificacion_clase_contratista') ?>").value);
+            }
+            if (document.getElementById("<?php echo $this->campoSeguro('porcentaje_clase_contratista') ?>").value != "") {
+                InfoPaso0.push(document.getElementById("<?php echo $this->campoSeguro('porcentaje_clase_contratista') ?>").value);
+            }
+
+            var arregloDatos = [];
             arregloDatos = JSON.stringify(InfoPaso0);
             AlmacenarPaso(arregloDatos);
         }
@@ -120,7 +174,7 @@ $urlDatosPaso = $url . $cadena2;
     }
 
     function AlmacenarPaso(arreglo) {
-    $.ajax({
+        $.ajax({
             url: "<?php echo $urlDatosPaso ?>",
             dataType: "json",
             data: {valor: arreglo},
